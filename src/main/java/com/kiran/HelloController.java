78fe2b6db1a143c4861c724731817276a22d70bf
Kiran Kompelli <kompellikiran15241@gmail.com>
2025-07-23 12:19:34 +0530

    rest demo manual

---
+++ src/main/java/com/kiran/HelloController.java
@@ -0,0 +1,15 @@
+package com.kiran;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+	@GetMapping("/")
+	public String greet() {
+		return "hello world";
+	}
+
+}
